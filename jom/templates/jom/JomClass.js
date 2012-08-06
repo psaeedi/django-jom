ddffc88440186f510a428167d21f9b62838d90a0
Michele Sama <michele.sama@gmail.com>
2012-08-06 17:13:43 +0100

    added debug info

--- jom/templates/jom/JomClass.js
+++ jom/templates/jom/JomClass.js
@@ -116,7 +116,22 @@
     	data: this.toMap(),
     	dataType: 'json',
     	type: 'POST',
-    	traditional: true,
+    	//traditional: true,
+    	beforeSubmit: function(formData, jqForm, options) { 
+    	    // formData is an array; here we use $.param to convert it to a string to display it 
+    	    // but the form plugin does this for you automatically when it submits the data 
+    	    var queryString = $.param(formData); 
+    	 
+    	    // jqForm is a jQuery object encapsulating the form element.  To access the 
+    	    // DOM element for the form do this: 
+    	    // var formElement = jqForm[0]; 
+    	 
+    	    alert('About to submit: \n\n' + queryString); 
+    	 
+    	    // here we could return false to prevent the form from being submitted; 
+    	    // returning anything other than false will allow the form submit to continue 
+    	    return true; 
+    	},
     	success: function(jsonResponse, statusText, xhr, $form) { 
     		if (jsonResponse.result == true) {
     			console.log(jsonResponse);
