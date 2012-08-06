44b4234de8fb9f534a720a0e5b989f61db1e2987
Michele Sama <michele.sama@gmail.com>
2012-08-06 21:43:02 +0100

    fixed a bug in init

--- jom/templates/jom/JomClass.js
+++ jom/templates/jom/JomClass.js
@@ -88,7 +88,7 @@
     		if (jsonResponse.result == true) {
     			// Update the current instance
     			// with the returned values.
-    			self.init(jsonResponse)
+    			self.init(jsonResponse.config);
     			successCallback(self);
     		} else {
     			errorCallback(jsonResponse);
@@ -115,22 +115,7 @@
 		url: {{ clazz|capital }}_ASYNC_UPDATE_URL,
     	dataType: 'json',
     	type: 'POST',
-    	//traditional: true,
-    	beforeSubmit: function(formData, jqForm, options) { 
-    	    // formData is an array; here we use $.param to convert it to a string to display it 
-    	    // but the form plugin does this for you automatically when it submits the data 
-    	    var queryString = $.param(formData); 
-    	 
-    	    // jqForm is a jQuery object encapsulating the form element.  To access the 
-    	    // DOM element for the form do this: 
-    	    // var formElement = jqForm[0]; 
-    	 
-    	    alert('About to submit: \n\n' + queryString); 
-    	 
-    	    // here we could return false to prevent the form from being submitted; 
-    	    // returning anything other than false will allow the form submit to continue 
-    	    return true; 
-    	},
+    	traditional: true,
     	success: function(jsonResponse, statusText, xhr, $form) { 
     		if (jsonResponse.result == true) {
     			console.log(jsonResponse);
